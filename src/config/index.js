4ddd65b26c62fe3ee1d9111bf076310accbca630
Diego.Lopez <diego.lopez@MacBookAir-DiegoLopez.local>
2025-10-14 23:22:15 -0500

    add modify email scope

--- src/config/index.js
+++ src/config/index.js
@@ -23,6 +23,7 @@ const config = {
     scopes: [
       "openid",
       "https://www.googleapis.com/auth/gmail.readonly",
+      "https://www.googleapis.com/auth/gmail.modify",
       "https://www.googleapis.com/auth/drive.readonly",
       "https://www.googleapis.com/auth/documents.readonly",
       "https://www.googleapis.com/auth/spreadsheets.readonly",
